561ebf21a9e1d5c11e84f710f8b354fff938bea5
rawatas <whattheshit@outlook.co.th>
2020-06-01 01:51:20 +0700

    create show payment

---
+++ resources/js/components/showData/showPayments.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="ft-cus">
+      <form-show-payment :data="paymentData" />
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import FormShowPayment from './formShowPayment.vue'
+export default Vue.extend({
+  name: 'showPayments',
+  components:{
+    FormShowPayment
+  },
+  data() {
+    return {
+        paymentData:[]
+    }
+  },
+  props:{
+    pawn_id: {
+        defult: ""
+    },
+  },
+  methods: {
+    async getData() {
+        let res = await window.api.get(`payment`)
+        console.log('aaa',res.data);
+        this.paymentData = res.data.payment
+    }
+  },
+  watch: {
+
+  },
+  mounted() {
+      this.getData()
+  },
+
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
