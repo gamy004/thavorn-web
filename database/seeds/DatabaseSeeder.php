8e4584d22b513ec4c26d67dec91feefacf541249
Vasuphat.put <narayutput@gmail.com>
2021-02-07 16:14:46 +0700

    - update PawnItemSeeder to random query ItemCategory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(RoleSeeder::class);
-        // $this->call(ItemCategorySeeder::class);
+        $this->call(ItemCategorySeeder::class);
         $this->call(ItemDamageSeeder::class);
-        // $this->call(PawnSeeder::class);
-        // $this->call(PawnItemSeeder::class);
-        // $this->call(PaymentSeeder::class);
+        $this->call(PawnSeeder::class);
+        $this->call(PawnItemSeeder::class);
+        $this->call(PaymentSeeder::class);
     }
 }
--- database/seeds/PawnItemSeeder.php
+++ database/seeds/PawnItemSeeder.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Pawn;
 use App\Models\PawnItem;
 use App\Models\ItemDamage;
+use App\Models\ItemCategory;
 
 class PawnItemSeeder extends Seeder
 {
@@ -27,9 +28,13 @@ class PawnItemSeeder extends Seeder
                 $mock_item_damage = $i % 2 === 0 ? 'ไม่มีตำหนิ' : 'มีตำหนิ';
                 $item_damage = ItemDamage::where('item_damage', $mock_item_damage)->first();
 
+                $mock_item_category_id = $i % 10;
+                $item_category = ItemCategory::where('id', $mock_item_category_id)->first();
+
                 factory(PawnItem::class)->create([
                     'pawn_id' => $pawn->id,
-                    'item_damage_id' => !is_null($item_damage) ? $item_damage->id : factory(ItemDamage::class)->create()->id
+                    'item_damage_id' => !is_null($item_damage) ? $item_damage->id : factory(ItemDamage::class)->create()->id,
+                    'item_category_id' => !is_null($item_category) ? $item_category->id : factory(ItemCategory::class)->create()->id,
                 ]);
             }
         }
